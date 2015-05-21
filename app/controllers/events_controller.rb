0098b6d6014304a8bc1abce12f69fb92e6f5f5b8
yancheng <zqalyc@163.com>
2015-05-21 18:36:46 +0800

    后台关系系统查询

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,8 @@ class EventsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   layout 'admin'
   def show
+    @events=nil?
+    @events=Event.order( "event_date ASC")
 
   end
 
@@ -10,11 +12,16 @@ class EventsController < ApplicationController
   end
 
   def create
-    event_date=params[:event][:event_date]
-     position=params[:event][:position]
+      event_date=params[:event][:event_date]
+     # position=params[:event][:position]
      content=params[:event][:content]
-     @event=Event.new(event_date:event_date,position:position,content:content)
-     @event.save
+     @event=Event.where(event_date: event_date).first
+     if  @event.nil?
+       @event=Event.new(event_date:event_date,content:content)
+       @event.save
+     else
+       @event.update_attributes(content:content)
+     end
      flash[:flag]="保存成功！"
      redirect_to :back
   end
--- db/migrate/20150519060853_create_events.rb
+++ db/migrate/20150519060853_create_events.rb
@@ -1,7 +1,7 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.date :event_date
+      t.string :event_date
       t.string :position
       t.text :content
       t.timestamps
