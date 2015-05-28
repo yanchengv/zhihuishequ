7edb98d445f919385703edfb8008e55025b3c434
yancheng <zqalyc@163.com>
2015-05-28 16:31:30 +0800

    weixin menu

--- app/assets/javascripts/weixin.js
+++ app/assets/javascripts/weixin.js
@@ -18,6 +18,5 @@
 //= require turbolinks
 //= require mobile/classie.js
 //= require mobile/modernizr.custom.js
-//= require mobile/jquery.mmenu.min.all
 //= require mobile/slick.min
 //require_tree .
